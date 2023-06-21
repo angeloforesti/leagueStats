8205d9fd4608f8b3467eff5f55c8bfad4778d09c
angeloforesti <angeloforestin@gmail.com>
2023-06-20 23:22:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LeagueStats</title>
+
+    
 </head>
 <body>
-<form method="post" action="scripts/query.php">
+<form method="post" action="scripts/queryAllDragons.php">
     <label for="objective">All Dragons per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
-<form method="post" action="scripts/averageQuery.php">
+<form method="post" action="scripts/queryAverageDragons.php">
     <label for="objective">average Dragons per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
-    
+<form method="post" action="scripts/queryAverageTime.php">
+    <label for="objective">average time per game</label>
+    <label for="league">League</label>
+    <input type="text" id="league" name="league">
+    <input type="submit" name="submit" id="submit" value="enviar">
+</form>
 
 </body>
 </html>
-
-
-
-
-
