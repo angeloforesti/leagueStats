706091b5c440f354283b36e9adcf0be76ea36f8b
angeloforesti <angeloforestin@gmail.com>
2023-06-21 22:44:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,39 +4,105 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.css">
+
     <title>LeagueStats</title>
 
     
 </head>
-<body>
-<form method="post" action="scripts/queryAllDragons.php">
+
+<body class="bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">League Stats</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quem Somos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="ligasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ligas</a>
+                        <ul class="dropdown-menu" aria-labelledby="ligasDropdown">
+                            <li><a class="dropdown-item" href="#">CBLOL</a></li>
+                            <li><a class="dropdown-item" href="#">LPL</a></li>
+                            <li><a class="dropdown-item" href="#">LCK</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Jogadores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Times</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<div class="container">
+<form method="post" action="scripts/queryAllDragons.php" class="dark-form">
     <label for="objective">All Dragons per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
-<form method="post" action="scripts/queryAverageDragons.php">
+<form method="post" action="scripts/queryAverageDragons.php" class="dark-form">
     <label for="objective">average Dragons per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
-<form method="post" action="scripts/queryAllTowers.php">
+<form method="post" action="scripts/queryAllTowers.php" class="dark-form">
     <label for="objective">towers per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
-<form method="post" action="scripts/queryAverageTowers.php">
+<form method="post" action="scripts/queryAverageTowers.php" class="dark-form">
     <label for="objective">average towers per game</label>
     <label for="league">League</label>
     <input type="text" id="league" name="league">
     <input type="submit" name="submit" id="submit" value="enviar">
 </form>
 
+<form method="post" action="scripts/queryAllKills.php" class="dark-form">
+    <label for="objective">kills per game</label>
+    <label for="league">League</label>
+    <input type="text" id="league" name="league">
+    <input type="submit" name="submit" id="submit" value="enviar">
+</form>
+
+
+<form method="post" action="scripts/queryAverageKills.php" class="dark-form">
+    <label for="objective">average kills per game</label>
+    <label for="league">League</label>
+    <input type="text" id="league" name="league">
+    <input type="submit" name="submit" id="submit" value="enviar">
+</form>
+
+<form method="post" action="scripts/queryAllTime.php" class="dark-form">
+    <label for="objective">All time per game</label>
+    <label for="league">League</label>
+    <input type="text" id="league" name="league">
+    <input type="submit" name="submit" id="submit" value="enviar">
+</form>
+
+<form method="post" action="scripts/queryAverageTime.php" class="dark-form">
+    <label for="objective">average time per game</label>
+    <label for="league">League</label>
+    <input type="text" id="league" name="league">
+    <input type="submit" name="submit" id="submit" value="enviar">
+</form>
+<div>
 
 
 </body>
